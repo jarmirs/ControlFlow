f8c34aa624a52cd67a76844836a8af014224ba8f
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-07-24 19:48:04 -0400

    2nd push

---
+++ src/Print.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Print {
+
+	public static void main(String[] args) {
+		public static void dispalyData() {
+			
+
+	}
+
+}
+	/*for (initialization; termination;
+     increment) {
+    statement(s)
+}
+
+//Example of looping print method
+//public static void displayList(List<String> values) {
+//for(int i=0;i<values.size();++i);{
+	//Systems.out.println(values.get[i]);
+}
